baa126c7ad03166d53bdcdb3db54668a671aa24a
vishwjeet <vrvishwujgare@gmail.com>
2022-11-17 09:42:56 +0530

    Added Day_1 code in CPP with output img

---
+++ Day_1/C++/Day_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string  c;
+
+    cout << "Enter an alphabet: ";
+    cin >> c;
+cout<<c;    
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'||c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+    {
+         cout << c << " is a vowel.";
+    }
+    else if(isalpha(c))//isaplha method is to check wheter value is alphabet or not 
+    {
+         cout << c << " is a consonant.";
+    }
+    else
+    {
+    cout << c << " is not a valid input.";
+    }
+    return 0;
+}
