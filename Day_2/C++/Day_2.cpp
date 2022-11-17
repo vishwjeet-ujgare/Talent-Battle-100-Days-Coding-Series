cff76eb5f12e03141b7f14ae068fdcf0e4522665
vishwjeet <vrvishwujgare@gmail.com>
2022-11-17 20:04:10 +0530

    Adde solution for day2 in CPP with output img

---
+++ Day_2/C++/Day_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main()
+{
+    char ch;
+    std::cout<<"Enter a character : ";
+    std::cin>>ch;
+
+    if((ch>='a' &&ch<='z') ||(ch>='A' &&ch<='Z'))
+    {
+        std::cout<<ch<<" is an alphabet";
+    }
+    else   
+    {
+        std::cout<<ch<<" is not an alphabet";
+    }
+    return 0;
+}
