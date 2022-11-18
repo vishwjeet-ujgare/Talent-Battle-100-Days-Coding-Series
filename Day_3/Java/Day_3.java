13e3556ba18a5b535909440b32ae953c3a9f6c13
vishwjeet <vrvishwujgare@gmail.com>
2022-11-18 23:28:39 +0530

    Added code in java for day-3 with output img
    -type casting (int)ch

---
+++ Day_3/Java/Day_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Day_3{
+    public static void main(String args[]) {
+        System.out.print("Enter a character : ");
+
+        Scanner sc = new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+
+  
+        System.out.println((int)ch);
+
+
+
+
+    }
+}
