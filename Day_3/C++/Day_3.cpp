849fc0032efd6438c74cb7a4022191a4b80ba393
vishwjeet <vrvishwujgare@gmail.com>
2022-11-18 23:06:34 +0530

    Adding code for day-3 with output img

---
+++ Day_3/C++/Day_3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout<<"Enter a character : ";
+    cin>>ch;
+
+    //converting char into int to get ascii value for respective charcter
+    cout<<(int)ch;
+}
