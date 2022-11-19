ea6091d5a0ffe5d549f4d5804408496063b2d96e
vishwjeet <vrvishwujgare@gmail.com>
2022-11-20 00:34:15 +0530

    added code for day 3 statement in java

---
+++ Day_4/Day_4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Day_4
+{
+    public static void main (String args[])
+    {
+        
+        System.out.print("Enter an integer number : ");
+      
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        
+        if(num==0)
+        {
+            System.out.println("Neither positive nor negative");
+        }
+        else if(num>0)
+        {
+            System.out.println("Positive number ");
+        }
+        else if(num<0)
+        {
+            System.out.println("Negative number ");
+            
+        }
+    }
+}
