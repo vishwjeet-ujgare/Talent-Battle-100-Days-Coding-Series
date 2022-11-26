0d9daf24757d871a56df3c18d3366e605193dcb3
vishwjeet <vrvishwujgare@gmail.com>
2022-11-26 21:41:17 +0530

    Day 9 solution in java formating

--- Day_9/Java/Day_9.java
+++ Day_9/Java/Day_9.java
@@ -2,29 +2,21 @@
 //Write a program to find Number of digits in an integer
 
 import java.util.Scanner;
-
-import javax.print.event.PrintEvent;
-
 public class Day_9 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter any natural number : ");
-        int num =sc.nextInt();
-        int temp=num;
+        int num = sc.nextInt();
+        int temp = num;
 
-       int count=0;
+        int count = 0;
 
-       while(num>0)
-       {
-        num=num/10;
-        count +=1;
-       }
-System.out.println("Number of Digits in "+temp+" : "+count);
-       
-
-
-
+        while (num > 0) {
+            num = num / 10;
+            count += 1;
+        }
+        System.out.println("Number of Digits in " + temp + " : " + count);
 
         sc.close();
 
