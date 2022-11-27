a00b0a5f47dcd858d55e79b51ac2e4dc8e348d79
vishwjeet <vrvishwujgare@gmail.com>
2022-11-27 08:52:11 +0530

    uploading Day 10 =>finding factorial of a numer .coding solution in java

--- Day_10/Java/Day_10.java
+++ Day_10/Java/Day_10.java
@@ -1,5 +1,29 @@
-package Day_10.Java;
+// Day 10 coding Statement: 
+// Write a program to find Factorial of a number
+
+import java.util.Scanner;
 
 public class Day_10 {
-    
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter Number : ");
+      int num = sc.nextInt();
+
+      if (num == 0) {
+         System.out.println("Factorial of " + num + " : 1");
+         sc.close();
+         return;
+      }
+
+      int temp = num;
+      int fact = 1;
+      
+      for (int i = num; i >= 1; i--) {
+         fact = fact * i;
+      }
+
+      System.out.println("Factorial of number " + temp + " : " + fact);
+      sc.close();
+   }
 }
