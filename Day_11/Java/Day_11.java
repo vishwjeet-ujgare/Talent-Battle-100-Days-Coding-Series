cbc80c727c0cc33006ce3f10f85b074627a3b3e2
vishwjeet <vrvishwujgare@gmail.com>
2022-11-27 09:38:38 +0530

    Day 11 fibonacci series solution in java

--- Day_11/Java/Day_11.java
+++ Day_11/Java/Day_11.java
@@ -1,5 +1,31 @@
-package Day_11.Java;
+
+// Day 11 coding Statement: 
+// Write a to find Fibonacci series up to n
+import java.util.Scanner;
 
 public class Day_11 {
-    
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter number : ");
+    int temp = sc.nextInt();
+    int num=temp-2;
+
+    int a = 0;
+    int b = 1;
+
+    System.out.println("Fibonacci Series Upto " + temp + " are : ");
+    System.out.print(a + " " + b);
+
+    for (int i = 0; i < num; i++) {
+      // swapping logic
+      int c = a + b;
+      System.out.print(" " + c);
+      a = b;
+      b = c;
+    }
+    sc.close();
+
+  }
 }
