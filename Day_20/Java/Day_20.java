73afd6fb7e6939f756d3efcbaf7e35c6871c9bf4
vishwjeet <vrvishwujgare@gmail.com>
2022-12-05 20:20:09 +0530

    Done with day 20 coding statement

---
+++ Day_20/Java/Day_20.java
@@ -0,0 +1,43 @@
+// Day 20 coding Statement : 
+// Write a program to identify if the number is Prime number or nots
+
+import java.util.Scanner;
+
+public class Day_20 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter A Number : ");
+	int num=sc.nextInt();
+
+	if(num==1)
+	{
+		System.out.println(num+" is not a Prime number.");
+		return ;
+	}
+	else
+	{
+		boolean isArmstrong=true;
+		for(int i =2;i<=num/2;i++)
+		{
+			if(num%i==0)
+			{
+				isArmstrong=false;
+				break;
+			}
+			
+		}
+
+		if(isArmstrong)
+		{
+			System.out.println(num+" is an Prime number.");
+
+		}else{
+			System.out.println(num+" is not a Prime number.");
+		}
+
+	}
+	
+
+        
+    }
+}
