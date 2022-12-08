72364098c0a05600eafecb32f4e992e5a7ba539c
vishwjeet <vrvishwujgare@gmail.com>
2022-12-08 21:16:28 +0530

    Added two solution for day 22 problem statement day_22 yet to be optimize

--- Day_22/Java/Day_22.java
+++ Day_22/Java/Day_22.java
@@ -1,7 +1,38 @@
+
+// Day 22 coding Statement : 
+// Write a program to express a number as a sum of two prime numbers
 import java.util.Scanner;
 
 public class Day_22 {
     public static void main(String[] args) {
-                
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+
+        for (int i = 2; i <= num; i++) {
+            boolean isPrime = true;
+            for (int j = 3; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+
+                for (int k = i; k <= num; k++) {
+                    boolean isPrime1 = true;
+                    for (int l = 2; l <= k/2; l++) {
+                        if (k % l == 0) {
+                            isPrime1 = false;
+                            break;
+                        }
+                    }
+                    if (isPrime1 && (i + k == num)) {
+                        System.out.println(num+" can be expressd as "+i + " + " + k + " = " + num);
+                    }
+                }
+            }
+        }
     }
 }
