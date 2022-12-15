9be8b2de6725f1cc12a21c423293e3876387fdae
vishwjeet <vrvishwujgare@gmail.com>
2022-12-15 16:12:03 +0530

    day 29

---
+++ Day_29/Java/Day_29.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import javax.swing.text.StyledEditorKit;
+public class Day_29 {
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    System.out.print("Enter First String : ");
+    String str1=sc.nextLine();
+
+    System.out.print("Enter Second String: ");
+    String str2=sc.nextLine();
+
+    System.out.println(str1+str2);
+
+
+ }
+}
